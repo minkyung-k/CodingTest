4db6a83b6646e62d8559a7b5af65f428dc7b182a
Lani <minkyung.k@gmail.com>
2022-08-20 16:38:42 +0900

    solution

--- Greedy/great numbers.py
+++ Greedy/great numbers.py
@@ -1,1 +1,24 @@
 # 큰 수의 법칙
+# 배열의 크기 N, 숫자가 더해지는 횟수 M, 그리고 K가 주어질 때 큰 수의 법칙에 따른 결과 출력
+
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+
+while True :
+    for i in range(k):
+        if m==0:
+            break
+        result+=first
+        m-=1
+    if m==0:
+        break
+    result+=second
+    m-=1
+
+print(result)
