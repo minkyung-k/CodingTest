596aa269d5c5bc9273e00ca6014bbd676beacc69
Lani <minkyung.k@gmail.com>
2022-08-21 16:13:49 +0900

    modify n

--- Greedy/pg 312.py
+++ Greedy/pg 312.py
@@ -7,8 +7,8 @@ result = arr[0] #첫 번째 숫자 지정
 for i in range(1,len(arr)) : #첫 번째 숫자는 이미 지정했으므로 반복문은 arr[1]부터 시작
     n = arr[i]
     if result <=1 or n<=1 : # 두 숫자 중 하나라도 0 또는 1이면 + 연산 사용
-        result += arr[i]
+        result += n
     else :
-        result *= arr[i]
+        result *= n
 
 print(result)
