51fe050c99d5763cf929b95af600457a6ae06c59
Lani <minkyung.k@gmail.com>
2022-08-21 16:05:06 +0900

    add comment

--- Greedy/pg 312.py
+++ Greedy/pg 312.py
@@ -1,12 +1,12 @@
 # 이코테 Q2 곱하기 혹은 더하기
 
-arr = list(map(int,input()))
+arr = list(map(int,input())) #문자열 입력받기
 
-result = arr[0]
+result = arr[0] #첫 번째 숫자 지정
 
-for i in range(1,len(arr)) :
+for i in range(1,len(arr)) : #첫 번째 숫자는 이미 지정했으므로 반복문은 arr[1]부터 시작
     n = arr[i]
-    if result <=1 or n<=1 :
+    if result <=1 or n<=1 : # 두 숫자 중 하나라도 0 또는 1이면 + 연산 사용
         result += arr[i]
     else :
         result *= arr[i]
